b9dcecf3f9e8d9655c5911a666e33d42e284f7a7
lmilunovic <milunovic.ladislav@gmail.com>
2024-04-21 18:52:31 +0200

    Initial commit, spring project setup

---
+++ src/test/java/com/example/udasapp/UdasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
